a26a936bb9ff218498ee2bfb60f58a7b62053aea
Pavel Lakhno <lmr_1990@mail.ru>
2025-11-12 13:48:21 +0300

    Initial Commit

---
+++ RecipeLoader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecipeLoader
+//
+//  Created by user on 12.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
